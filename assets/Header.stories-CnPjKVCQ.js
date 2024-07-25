7664745796b2647399dc2bfaba10969704c560fa
roman-16 <roman@lerchster.dev>
2024-07-25 13:25:47 +0000

    Deploying to gh-pages from @ blue-tomato/blueto@a3d58532963fe4cc39912483b151a0f5b3734604 🚀

---
+++ assets/Header.stories-CnPjKVCQ.js
@@ -0,0 +1,7 @@
+import{f as o}from"./Button-CIWwi49D.js";import{H as p}from"./Header-CBlyk3og.js";import"./index-DZLKizrv.js";import"./index-RYns6xqu.js";import"./index-DNUR7M9R.js";const f={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: "Jane Doe"
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const L=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,L as __namedExportsOrder,f as default};
