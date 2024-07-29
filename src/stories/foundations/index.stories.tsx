1c121c3ab52ccd6c2823b703a53d7d648449eccb
Roman <roman.lerchster@blue-tomato.com>
2024-07-29 18:12:17 +0200

    add colors

---
+++ src/stories/foundations/index.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ColorsStory from "./ColorsStory";
+
+const Colors: StoryObj = {
+	render: ColorsStory,
+};
+
+const meta: Meta = {
+	title: "Foundations",
+};
+
+export default meta;
+export { Colors };
