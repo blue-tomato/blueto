b801b5634c0830627e86e0fc83cbd9e76e80b7bd
roman-16 <roman@lerchster.dev>
2024-11-25 17:14:25 +0000

    Deploying to gh-pages from @ blue-tomato/blueto@63910a2335aa9fbb270b72b127f4754a8b206ea1 🚀

---
+++ assets/Button.stories-jGzL8_NR.js
@@ -0,0 +1,5 @@
+import{B as i}from"./SizeButton-BfShjyY5.js";import"./index-590eavWx.js";import"./index-RYns6xqu.js";const l={title:"components/Button",component:i,args:{children:"Button",disabled:!1,icon:void 0,loading:!1,variant:"primary"}},o={},r={args:{icon:"flag.at"}};var a,t,e;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(e=(t=o.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};var s,n,c;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    icon: "flag.at"
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const u=["Default","WithIcon"];export{o as Default,r as WithIcon,u as __namedExportsOrder,l as default};
---
+++ assets/preview-DGYEdzvg.js
@@ -0,0 +1,1 @@
+const e={parameters:{layout:"centered"},tags:["autodocs"]};export{e as default};
