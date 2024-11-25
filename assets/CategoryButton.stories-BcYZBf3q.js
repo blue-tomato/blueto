ca3683ca39e628e7c9ff1a4fdcff7b809df8aabd
roman-16 <roman@lerchster.dev>
2024-11-25 11:56:39 +0000

    Deploying to gh-pages from @ blue-tomato/blueto@382247a87dfa6f53e95a414eef06850c340a0bc9 🚀

---
+++ assets/CategoryButton.stories-BcYZBf3q.js
@@ -0,0 +1,8 @@
+import{C as a}from"./SizeButton-Dh5k9opX.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./index-Dejnh_W_.js";const l={title:"components/CategoryButton",component:a,parameters:{layout:"centered"},tags:["autodocs"]},t={args:{active:!1,children:"CategoryButton",color:"white",size:"large"}};var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    active: false,
+    children: "CategoryButton",
+    color: "white",
+    size: "large"
+  }
+}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const m=["Default"];export{t as Default,m as __namedExportsOrder,l as default};
---
+++ assets/SizeButton.stories-CUN0PCCn.js
@@ -0,0 +1,7 @@
+import{S as s}from"./SizeButton-Dh5k9opX.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./index-Dejnh_W_.js";const d={title:"components/SizeButton",component:s,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{active:!1,children:"46",disabled:!1}};var t,a,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    active: false,
+    children: "46",
+    disabled: false
+  }
+}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,d as default};
