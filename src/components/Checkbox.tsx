755ac580111b968ed38274aede5e388ea33d672f
Roman <r.lerchster@blue-tomato.com>
2024-11-27 17:17:15 +0100

    Add inputProps

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,20 +5,35 @@ import Icon from "./Icon";
 
 type Props = React.HTMLAttributes<HTMLDivElement> & {
 	error?: React.ReactNode;
+	inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
 };
 
-const Checkbox = ({ children, className, error = false, ...props }: Props) => {
-	const [checked, setChecked] = useState(false);
+const Checkbox = ({
+	children,
+	className,
+	error = false,
+	inputProps,
+	...props
+}: Props) => {
+	const [checked, setChecked] = useState(inputProps?.checked ?? false);
 
 	return (
 		<div className={classNames(className, styles.wrapper)} {...props}>
 			<label className={styles.label}>
 				<div className={classNames(styles.box, checked && styles.boxChecked)}>
 					<input
-						checked={checked}
-						className={classNames(styles.input, error && styles.inputError)}
-						onChange={(event) => setChecked(event.target.checked)}
-						type="checkbox"
+						{...inputProps}
+						checked={inputProps?.checked ?? checked}
+						className={classNames(
+							inputProps?.className,
+							styles.input,
+							error && styles.inputError,
+						)}
+						onChange={(event) => {
+							inputProps?.onChange?.(event);
+							setChecked(event.target.checked);
+						}}
+						type={inputProps?.type ?? "checkbox"}
 					/>
 					<Icon
 						className={classNames(
