03ceb9881aed084880b9581699ee694b652f2330
roman-16 <roman@lerchster.dev>
2025-01-09 13:21:48 +0000

    Deploying to gh-pages from @ blue-tomato/blueto@217ef415e3e274b0466cb0b150beb09bb20bfa04 🚀

--- assets/Checkbox.stories-DzNDSlya.js
+++
@@ -1,5 +0,0 @@
-import{a as p}from"./SizeButton-Wcfan_9j.js";import"./index-590eavWx.js";import"./index-RYns6xqu.js";const u={title:"components/Checkbox",component:p,args:{children:"Checkbox",error:void 0},argTypes:{error:{control:"text"}}},r={},o={args:{error:"Error"}};var e,s,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var a,c,n;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    error: "Error"
-  }
-}`,...(n=(c=o.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const l=["Default","WithError"];export{r as Default,o as WithError,l as __namedExportsOrder,u as default};
