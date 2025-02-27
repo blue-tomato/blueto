baf9b28147ee7bb350dec278b300e01989e3fa77
roman-16 <roman@lerchster.dev>
2025-02-27 13:47:14 +0000

    Deploying to gh-pages from @ blue-tomato/blueto@1620ecf504616d19265d5875cc6225f2543bec17 🚀

--- assets/preview-CIaZ9nQ8.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-DEdD30eg.js";import{ai as i}from"./index-Bqf_1ug8.js";import"./index-RYns6xqu.js";import"./iframe-DKZpDXBR.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./isArray-AquwOF00.js";import"./index-DrFu-skq.js";const a=({children:n,...t})=>{const r=t.context.primaryStory.parameters.links;return e.jsxs(i,{...t,children:[r&&e.jsxs("div",{className:"docs-links",children:[e.jsx("a",{href:`https://blue-tomato.atlassian.net/wiki/x/${r.confluence}`,target:"_blank",rel:"noreferrer",children:"Confluence"}),e.jsx("a",{href:`https://www.figma.com/design/${r.figma}`,target:"_blank",rel:"noreferrer",children:"Figma"})]}),n]})};a.__docgenInfo={description:"",methods:[],displayName:"DocsLinks",props:{context:{required:!0,tsType:{name:"signature",type:"object",raw:"{ primaryStory: { parameters: StoryMetaParameters } }",signature:{properties:[{key:"primaryStory",value:{name:"signature",type:"object",raw:"{ parameters: StoryMetaParameters }",signature:{properties:[{key:"parameters",value:{name:"signature",type:"object",raw:`{
-	links?: {
-		confluence: string;
-		figma: string;
-	};
-}`,signature:{properties:[{key:"links",value:{name:"signature",type:"object",raw:`{
-	confluence: string;
-	figma: string;
-}`,signature:{properties:[{key:"confluence",value:{name:"string",required:!0}},{key:"figma",value:{name:"string",required:!0}}]},required:!1}}]},required:!0}}]},required:!0}}]}},description:""}}};const y={parameters:{docs:{container:a},layout:"padded"},tags:["autodocs"]};export{y as default};
