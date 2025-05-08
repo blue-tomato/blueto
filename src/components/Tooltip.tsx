34c34714be86233a1e6cf2c23ca196051bdbb8d9
anna.toeplitz <anna.toeplitz@blue-tomato.com>
2025-05-08 14:40:56 +0200

    small improvement

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,14 +1,27 @@
 import { forwardRef } from "react";
 import { Tooltip as ReactTooltip, type TooltipRefProps } from "react-tooltip";
+import styles from "./Tooltip.module.scss";
 
 type Props = {
 	children?: React.ReactNode;
 	anchorSelect?: string;
+	maxWidth?: string;
+	type?: "yellow" | "black";
 };
 
 const Tooltip = forwardRef<TooltipRefProps, Props>(
-	({ children, anchorSelect }, ref) => (
-		<ReactTooltip ref={ref} anchorSelect={anchorSelect}>
+	({ children, anchorSelect, maxWidth, type }, ref) => (
+		<ReactTooltip
+			ref={ref}
+			anchorSelect={anchorSelect}
+			offset={8}
+			className={styles.backgroundYellow}
+			style={{
+				backgroundColor: type === "yellow" ? "#fff5b3" : "#333333",
+				color: type === "yellow" ? "#333333" : "#ffffff",
+				maxWidth: maxWidth,
+			}}
+		>
 			{children}
 		</ReactTooltip>
 	),
