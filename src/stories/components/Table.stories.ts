d0aac4042dffdd48671a23c8bbb84b2e9b563f2c
anna.toeplitz <anna.toeplitz@blue-tomato.com>
2025-09-02 17:07:34 +0200

    add basic

---
+++ src/stories/components/Table.stories.ts
@@ -0,0 +1,18 @@
+import {Table} from "@/components";
+import type { StoryMeta } from "@/types";
+
+const Docs: StoryMeta<typeof Table> = {
+    title: "BLUETO/components/Table",
+    component: Table,
+    parameters: {
+        links: {
+            confluence: "p4BoO",
+            figma: "rUIq4O2W7nCzofq3nFoURP/BLUETO-Components?node-id=466-32",
+        },
+    },
+};
+export default Docs;
+const Default: StoryMeta<typeof Table> = {
+};
+
+export { Default };
